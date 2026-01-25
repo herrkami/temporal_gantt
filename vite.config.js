26a5d8adadaff0aa1c9234c704f23d73fc06adc6
herrkami <aebsor@gmail.com>
2026-01-25 13:01:41 +0100

    Add debug build mode

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.js'),
             name: 'Gantt',
             fileName: 'frappe-gantt',
         },
+        minify: mode !== 'debug',
+        sourcemap: mode === 'debug',
         rollupOptions: {
             output: {
                 format: 'cjs',
@@ -18,4 +20,4 @@ export default defineConfig({
     },
     output: { interop: 'auto' },
     server: { watch: { include: ['dist/*', 'src/*'] } }
-});
+}));
